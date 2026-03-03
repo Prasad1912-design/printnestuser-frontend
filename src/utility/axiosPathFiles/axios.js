f24de8105f62af4ada15a1df15a1ddbcd3a52bf8
Prasad1912-design <moreprasad1836@gmail.com>
2026-03-03 22:45:59 +0530

    LFsS

--- src/utility/axiosPathFiles/axios.js
+++ src/utility/axiosPathFiles/axios.js
@@ -1,8 +1,24 @@
+// axiosPathFiles/axios.js
 import axios from "axios";
 
-// Common File
-
-export default axios.create({
-    baseURL : process.env.REACT_APP_BACKEND_URL,
-    withCredentials: true
+const instance = axios.create({
+  baseURL: process.env.REACT_APP_BACKEND_URL || "http://localhost:3005",
+  headers: {
+    "Content-Type": "application/json",
+  },
+  withCredentials: true, // optional if you are using cookies
 });
+
+// 🔹 Add token automatically before each request
+instance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("accessToken"); // get token
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`; // attach it
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
+export default instance;
