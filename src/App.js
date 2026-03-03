63610bfac59212850859679d1fc1d6cb14016d71
Prasad1912-design <moreprasad1836@gmail.com>
2026-03-03 17:37:13 +0530

    SSS

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
 import React from "react";
-import { BrowserRouter } from "react-router-dom";
-import Approutes from "./Approutes/Approutes";
-import CreateContext from "./Context/CreateContext";
-import interceptor from "./utility/interceptor"; // The line where you written this the code inside the file is running
 
 function App() {
   return (
-    <BrowserRouter>
-      <CreateContext>
-        <Approutes />
-      </CreateContext>
-    </BrowserRouter>
+    <div style={{ textAlign: "center", marginTop: "50px", fontSize: "24px" }}>
+      Hello World from PrintNest Frontend 🚀
+    </div>
   );
 }
 
