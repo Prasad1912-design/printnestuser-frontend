f999a6c5cc5e28b8af59cadd7f82ee26757382eb
Prasad1912-design <moreprasad1836@gmail.com>
2026-03-03 21:51:08 +0530

    LFNT

--- src/Components/header_al.js
+++ src/Components/header_al.js
@@ -90,7 +90,7 @@ const { cartCounts, fetchCartCount } = useCount();
         console.log("Logout Order History Page");
         console.log(err.response.status);
         localStorage.removeItem('accessToken');
-        logout();
+        // logout();
       }
     })
 
@@ -109,7 +109,7 @@ const { cartCounts, fetchCartCount } = useCount();
     setTimeout(()=>{
       setLogoutStatus(false);
       localStorage.removeItem('accessToken');
-      logout();
+      // logout();
     },2000);
   }
 
