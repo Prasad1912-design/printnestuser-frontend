29301e6501e502a2b4178c9159b731bbbb76b720
Prasad1912-design <moreprasad1836@gmail.com>
2026-03-03 16:28:08 +0530

    Done with updatesss

--- src/utility/interceptor.js
+++ src/utility/interceptor.js
@@ -1,13 +1,15 @@
-import axios from "./axiosPathFiles/axios";
+import axios from 'axios';
 
-axios.interceptors.request.use((config)=>{
+const instance = axios.create({
+  baseURL: 'https://printnestuser-backend.onrender.com/' // This should point to your Render backend
+});
+
+instance.interceptors.request.use((config)=>{
   const token = localStorage.getItem('accessToken');
-
-  if(token)
-  {
+  if(token){
     config.headers.Authorization = `Bearer ${token}`;
   }
-
   return config;
-}, (error)=> Promise.reject(error)
-);
+}, (error)=> Promise.reject(error));
+
+export default instance;
